e3ed9acdb0d6ba698ce9ec542ad709b7a05eb9c9
Piotr Fusik <fox@scene.pl>
2021-11-27 17:56:04 +0100

    Paint.NET plugin. Decoding only for now.

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 CFLAGS = -O2 -Wall
+CSC = "C:/Program Files/Microsoft Visual Studio/2022/Community/MSBuild/Current/Bin/Roslyn/csc.exe" -nologo
+PAINT_NET_DIR = C:/Program Files/paint.net
+DOTNET_REF_DIR = C:/Program Files/dotnet/packs/Microsoft.NETCore.App.Ref/6.0.0/ref/net6.0
 TRANSPILED_LANG = c cpp cs java js py swift
 TRANSPILED_ENCODER = $(TRANSPILED_LANG:%=transpiled/QOIEncoder.%)
 TRANSPILED_DECODER = $(TRANSPILED_LANG:%=transpiled/QOIDecoder.%)
@@ -8,6 +11,9 @@ all: png2qoi $(TRANSPILED_ENCODER) $(TRANSPILED_DECODER)
 png2qoi: png2qoi.c transpiled/QOIEncoder.c transpiled/QOIDecoder.c
 	$(CC) $(CFLAGS) -I transpiled -o $@ $^ -lpng
 
+QOIPaintDotNet.dll: QOIPaintDotNet.cs transpiled/QOIDecoder.cs
+	$(CSC) -o+ -out:$@ -t:library $^ -nostdlib -r:"$(PAINT_NET_DIR)/PaintDotNet.Data.dll" -r:"$(PAINT_NET_DIR)/System.Drawing.Common.dll" -r:"$(DOTNET_REF_DIR)/System.Drawing.dll" -r:"$(DOTNET_REF_DIR)/System.Runtime.dll"
+
 $(TRANSPILED_ENCODER): QOIEncoder.ci
 	mkdir -p $(@D) && cito -o $@ $^
 
@@ -15,6 +21,6 @@ $(TRANSPILED_DECODER): QOIDecoder.ci
 	mkdir -p $(@D) && cito -o $@ $^
 
 clean:
-	$(RM) png2qoi $(TRANSPILED_ENCODER) $(TRANSPILED_DECODER)
+	$(RM) png2qoi QOIPaintDotNet.dll $(TRANSPILED_ENCODER) $(TRANSPILED_DECODER)
 
 .PHONY: all clean
---
+++ QOIPaintDotNet.cs
@@ -0,0 +1,80 @@
+﻿// QOIPaintDotNet.cs - QOI Paint.NET plugin
+//
+// Copyright (C) 2021 Piotr Fusik
+//
+// MIT License:
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+using PaintDotNet;
+
+[assembly: AssemblyTitle("Paint.NET Quite OK Image (QOI) plugin")]
+[assembly: AssemblyCompany("Piotr Fusik")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+
+namespace QOI.PaintDotNet
+{
+	class QOIFileType : FileType
+	{
+		public QOIFileType() : base("Quite OK Image", new FileTypeOptions { LoadExtensions = new string[] { ".qoi" } })
+		{
+		}
+
+		protected override Document OnLoad(Stream input)
+		{
+			// Read
+			long longLength = input.Length;
+			if (longLength > 0x7fffffc7) // max byte array length according to https://docs.microsoft.com/en-us/dotnet/api/system.array?view=net-6.0
+				throw new Exception("File too long");
+			int encodedLength = (int) longLength;
+			byte[] encoded = new byte[encodedLength];
+			encodedLength = input.Read(encoded, 0, encodedLength);
+
+			// Decode
+			QOIDecoder qoi = new QOIDecoder();
+			if (!qoi.Decode(encoded, encodedLength))
+				throw new Exception("Decoding error");
+			int width = qoi.GetWidth();
+
+			// Pass to Paint.NET
+			GCHandle pinnedPixels = GCHandle.Alloc(qoi.GetPixels(), GCHandleType.Pinned);
+			using (Bitmap bitmap = new Bitmap(width, qoi.GetHeight(), width << 2, PixelFormat.Format32bppArgb, pinnedPixels.AddrOfPinnedObject())) {
+				pinnedPixels.Free();
+				return Document.FromImage(bitmap);
+			}
+		}
+	}
+
+	public class QOIFileTypeFactory : IFileTypeFactory
+	{
+		public FileType[] GetFileTypeInstances()
+		{
+			return new FileType[] { new QOIFileType() };
+		}
+	}
+}
