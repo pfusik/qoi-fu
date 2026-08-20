8356b9e3c5348dab4dbbec9d055f7b1cb83c97df
Piotr Fusik <piotr@fusion-lang.org>
2026-08-20 09:01:11 +0200

    New win32 signing certificate.

--- win32/win32.mk
+++ win32/win32.mk
@@ -5,7 +5,7 @@ CXX32 = i686-w64-mingw32-g++
 CSC = "C:/Program Files/Microsoft Visual Studio/2022/Community/MSBuild/Current/Bin/Roslyn/csc.exe" -nologo
 PAINT_NET_DIR = C:/Program Files/paint.net
 DOTNET_REF_DIR = `echo C:/Program\ Files/dotnet/packs/Microsoft.NETCore.App.Ref/9.0.*/ref/net9.0`
-DO_SIGN = signtool sign -d "Quite OK Image plugins $(VERSION)" -n "Open Source Developer, Piotr Fusik" -tr http://time.certum.pl -fd sha256 -td sha256 $^ && touch $@
+DO_SIGN = signtool sign -d "Quite OK Image plugins $(VERSION)" -n "Open Source Developer Piotr Fusik" -tr http://time.certum.pl -fd sha256 -td sha256 $^ && touch $@
 
 win32/wicqoi64.dll: win32/wicqoi.cpp win32/QOI.o
 	$(CXX) $(CXXFLAGS) -I transpiled -o $@ $^ -shared -lwindowscodecs -lole32
